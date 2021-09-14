6f1306e93b1ce46cf711ed17a2f7cb6c3ebfb3cf
Boia Alexandru <alexandru.boia@outlook.com>
2021-09-14 22:27:45 +0300

    Updated imports

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,5 +1,7 @@
 import PropertySheet from './PropertySheet.jsx';
+import PropertySheetLabelAlignments from './PropertySheetLabelAlignments.js';
 
 export {
+	PropertySheetLabelAlignments,
 	PropertySheet
 };
